c8c37e10b6efe3dbb8e3b048ed47d6f0543fa35a
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-09-04 10:16:53 -0500

    firebase credientials inserted

--- app/app.js
+++ app/app.js
@@ -2,3 +2,25 @@
 
 const app = angular.module('Mushrooms', ['ngRoute']);
 
+// app.config(($routeProvider) => {
+
+// 	//add partial for mushroom list when factory is built
+// 	$routeProvider
+// 	.when('/')
+// 	.otherwise('/');
+
+// });
+
+app.run(($location, FBCreds) => {
+	let authConfig = {
+		apiKey: FBCreds.apiKey,
+		authDomain: FBCreds.authDomain,
+	    databaseURL: FBCreds.databaseURL,
+	};
+
+	console.log("authConfig", authConfig);
+
+	firebase.initializeApp(authConfig);
+
+});
+
